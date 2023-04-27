955d10019c25ac00f0f7adfd691c928417ab2898
Atte Valtonen <atte.hj.valtonen@gmail.com>
2023-04-27 23:25:55 +0300

    Migrated old urlconf style into new

--- example_app/urls.py
+++ example_app/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.urls import path
+
 from example_app.views import (
     OrderListCreateEndpoint, OrderRetrieveUpdateDestroyEndpoint, OrderRetrieveUpdateEndpoint,
     OrderCreateWithGenericEndpoint, OrderListWithGenericEndpoint, OrderRetrieveWithGenericEndpoint,
@@ -9,77 +10,77 @@ from example_app.views import (
 
 
 urlpatterns = [
-    url(
-        r'^orders-list-without-read-serializer/$',
+    path(
+        'orders-list-without-read-serializer/',
         OrderListWithoutReadSerializerEndpoint.as_view(),
         name='list_without_request_serializer'
     ),
-    url(
-        r'^orders/$',
+    path(
+        r'orders/',
         OrderListCreateEndpoint.as_view(),
         name='list_create'
     ),
-    url(
-        r'^orders/(?P<pk>[0-9]+)$',
+    path(
+        r'orders/<int:pk>/',
         OrderRetrieveUpdateDestroyEndpoint.as_view(),
         name='retrieve_update_destroy'
     ),
-    url(
-        r'^orders-retrieve-update/(?P<pk>[0-9]+)$',
+    path(
+        r'orders-retrieve-update/<int:pk>/',
         OrderRetrieveUpdateEndpoint.as_view(),
         name='retrieve_update'
     ),
-    url(
-        r'^orders-list-generic-view/$',
+    path(
+        r'orders-list-generic-view/',
         OrderListWithGenericEndpoint.as_view(),
         name='list'
     ),
-    url(
-        r'^orders-retrieve-generic-view/(?P<pk>[0-9]+)$',
+    path(
+        r'orders-retrieve-generic-view/<int:pk>/',
         OrderRetrieveWithGenericEndpoint.as_view(),
         name='retrieve'
     ),
-    url(
-        r'^orders-create-generic-view/$',
+    path(
+        r'orders-create-generic-view/',
         OrderCreateWithGenericEndpoint.as_view(),
         name='create'
     ),
-    url(
-        r'^orders-update-generic-view/(?P<pk>[0-9]+)$',
+    path(
+        r'orders-update-generic-view/<int:pk>/',
         OrderUpdateWithGenericEndpoint.as_view(),
         name='update'
     ),
-    url(
-        r'^orders-list-mixin/$',
+    path(
+        r'orders-list-mixin/',
         OrderListWithMixinEndpoint.as_view(),
         name='list_mixin'
     ),
-    url(
-        r'^orders-retrieve-mixin/(?P<pk>[0-9]+)$',
+    path(
+        r'orders-retrieve-mixin/<int:pk>/',
         OrderRetrieveWithMixinEndpoint.as_view(),
         name='retrieve_mixin'
     ),
-    url(
-        r'^orders-create-mixin/$',
+    path(
+        r'orders-create-mixin/',
         OrderCreateWithMixinEndpoint.as_view(),
         name='create_mixin'
     ),
-    url(
-        r'^orders-update-mixin/(?P<pk>[0-9]+)$',
+    path(
+        r'orders-update-mixin/<int:pk>/',
         OrderUpdateWithMixinEndpoint.as_view(),
         name='update_mixin'
     ),
 
-    url(
-        r'^orders-viewset/$',
+    path(
+        r'orders-viewset/',
         OrderViewset.as_view({
             'get': 'list',
             'post': 'create',
         }),
         name='viewset_list_create'
     ),
-    url(
-        r'^orders-viewset/(?P<pk>[0-9]+)$',
+    path(
+        r'orders-viewset/<int:pk>/',
         OrderViewset.as_view({
             'get': 'retrieve',
             'put': 'update',
