d98a316e6e31f651cc70dfe74e8d20a474674be3
Atte Valtonen <atte.hj.valtonen@gmail.com>
2023-04-27 23:35:25 +0300

    Classifiers fixed

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ setup(
         "Framework :: Django",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
-        "Framework :: Django :: 4.1"
-        "Framework :: Django :: 4.2"
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: Finnish",
